429bff8c2391ffc570d901fc129b0deefe1a2e9d
이윤혁 <yunhyeok@ip-192-168-5-2.ap-northeast-2.compute.internal>
2026-06-20 12:13:00 +0900

    chore: use nextConfig redirect instead of navigation redirect

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    async redirects() {
+        return [
+            {
+                source: '/',
+                destination: '/creator',
+                permanent: false,
+            },
+        ];
+    },
+};
 
 module.exports = nextConfig;
